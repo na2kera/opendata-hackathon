8e483e4b39c5eaade2b4966d22e21349049c37f9
na2kera <nat3mitaro@gmail.com>
2024-08-24 00:36:40 +0900

    名前が入力されていない時に名前を入力するようにする

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import Header from "@/components/Header";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // if (!user) {
+  //   return redirect("/login");
+  // }
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -14,6 +24,16 @@ export default async function Index() {
 
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
+        <main className="flex-1 flex flex-col gap-6">
+          {!user && (
+            <a
+              href="/login"
+              className="text-center text-xl font-semibold text-red-500 bg-red-100 py-4 px-6 rounded-lg shadow-md"
+            >
+              ログインしてください
+            </a>
+          )}
+        </main>
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">

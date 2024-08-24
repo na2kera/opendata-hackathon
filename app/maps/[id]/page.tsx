b16f2c5cf036e0e938f788e2793eaceed8a47846
na2kera <nat3mitaro@gmail.com>
2024-08-24 11:58:40 +0900

    詳細ページ

---
+++ app/maps/[id]/page.tsx
@@ -0,0 +1,34 @@
+import IndividualMap from "@/components/IndividualMap";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function MapPage({ params }: { params: { id: string } }) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  //idに合致するデータを取得する
+  const { data, error } = await supabase
+    .from("geojson_data")
+    .select("*")
+    .eq("id", params.id);
+  console.log("data", data);
+
+  if (error) {
+    console.log("error", error);
+  }
+
+  return (
+    <>
+      {data && data[0] && (
+        <IndividualMap individual_geojson_data={data[0] as Course} />
+      )}
+    </>
+  );
+}
---
+++ components/IndividualMap.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+type Props = {
+  individual_geojson_data: Course;
+};
+
+const IndividualMap: React.FC<Props> = ({ individual_geojson_data }) => {
+  return (
+    <>
+      {individual_geojson_data && (
+        <Typography variant="h4">{individual_geojson_data.title}</Typography>
+      )}
+    </>
+  );
+};
+
+export default IndividualMap;

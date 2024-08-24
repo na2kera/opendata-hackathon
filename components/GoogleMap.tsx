f222fceaf008d78d243c37f023cc766b5ce48d0e
yoshiddddd <yoshidakerapo@gmail.com>
2024-08-24 13:28:15 +0900

    ピンの色を白化させる

--- components/GoogleMap.tsx
+++ components/GoogleMap.tsx
@@ -1,95 +1,19 @@
-// "use client";
-
-// import { useEffect } from "react";
-// import Script from "next/script";
-// import { Loader } from '@googlemaps/js-api-loader';
-
-// export default function GoogleMap({ geojson_data }: { geojson_data: any }) {
-//   useEffect(() => {
-//     const initMap = () => {
-//       const map = new (window as any).google.maps.Map(
-//         document.getElementById("map"),
-//         {
-//           center: { lat: 35.706248, lng: 139.683761 }, // Tokyo coordinates
-//           zoom: 15,
-//         }
-//       );
-
-//       // デフォルトのピンに似たカスタムアイコンを設定する関数
-//       const getIcon = (color: string) => {
-//         return {
-//           url: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`, //ここについては任意に設定可能
-//           scale: 10,
-//         };
-//       };
-
-//       // GeoJSONデータを地図に追加
-//       if (geojson_data && geojson_data[0] && geojson_data[0].geo_json) {
-//         map.data.addGeoJson(geojson_data[0].geo_json);
-//       }
-//       // 初期スタイルを設定（デフォルトのピンアイコンに似せたカスタムアイコンを使用）
-//       map.data.setStyle({
-//         icon: getIcon("red"), // 初期色は青
-//       });
-
-//       // ピンがクリックされたときのイベントリスナーを追加
-//       map.data.addListener("click", (event: any) => {
-//         const feature = event.feature;
-//         const isClicked = feature.getProperty("clicked");
-
-//         if (isClicked) {
-//             console.log(feature)
-//           feature.setProperty("clicked", false);
-//           map.data.overrideStyle(feature, { icon: getIcon("red") }); 
-//         } else {
-//           feature.setProperty("clicked", true);
-//           map.data.overrideStyle(feature, { icon: getIcon("blue") });
-//         }
-//       });
-//     };
-
-//     // Google Maps APIがロード済みかチェック
-//     if ((window as any).google && (window as any).google.maps) {
-//       initMap();
-//     } else {
-//       (window as any).initMap = initMap;
-//     }
-//   }, [geojson_data]);
-
-//   return (
-//     <>
-//       <Script
-//         src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
-//         async
-//         defer
-//         onLoad={() => {
-//           if ((window as any).initMap) {
-//             (window as any).initMap();
-//           }
-//         }}
-//       />
-//       <div id="map" style={{ width: "100%", height: "600px" }}></div>
-//     </>
-//   );
-// }
-
 "use client";
 
 import { useEffect, useState } from "react";
 import Script from "next/script";
 import styles from './GoogleMap.module.css'; // モーダルのスタイル用にCSSをインポート
-import { getTextOfJSDocComment } from "typescript";
 
 export default function GoogleMap({ geojson_data }: { geojson_data: any }) {
   const [modalOpen, setModalOpen] = useState(false);
-  const [modalImage, setModalImage] = useState<string | null>(null);
+  const [modalImage, setModalImage] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     const initMap = () => {
       const map = new (window as any).google.maps.Map(
         document.getElementById("map"),
         {
-          center: { lat: 35.706248, lng: 139.683761 }, // Tokyo coordinates
+          center: { lat: 35.722627, lng: 139.6399039 }, // Tokyo coordinates
           zoom: 15,
         }
       );
@@ -98,28 +22,36 @@ export default function GoogleMap({ geojson_data }: { geojson_data: any }) {
       if (geojson_data && geojson_data[6] && geojson_data[6].geo_json) {
         map.data.addGeoJson(geojson_data[6].geo_json);
       }
-      console.log(geojson_data)
 
-        const getIcon = (color: string) => {
-            return{
-                url: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`,
-            }
-        }
+      // アイコンの色を設定する関数
+    //   https://www.single-life.tokyo/google-maps%EF%BC%88%E3%82%B0%E3%83%BC%E3%82%B0%E3%83%AB%E3%83%9E%E3%83%83%E3%83%97%EF%BC%89%E3%81%A7%E4%BD%BF%E3%81%88%E3%82%8B%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3/
+      const getIcon = (color: string) => {
+        return {
+          url: `http://maps.google.com/mapfiles/ms/icons/${color}-dot.png`,
+        };
+      };
+      map.data.setStyle({
+                icon: getIcon("red"), // 初期色は青
+              });
       // ピンがクリックされたときのイベントリスナーを追加
       map.data.addListener("click", (event: any) => {
         const feature = event.feature;
         const description = feature.getProperty("gx_media_links");
-        const color = feature.getProperty("marlker-color");
+        const color = feature.getProperty("marker-color"); // `marlker-color` を `marker-color` に修正
         const isClicked = feature.getProperty("clicked");
-        console.log(feature)
+
         if (description) {
-            // console.log(description)
           setModalImage(description); // モーダルに表示する画像を設定
           setModalOpen(true); // モーダルを開く
-          map.data.overrideStyle(feature, { icon: getIcon(color) });
         }
+
+        // if (isClicked) {
+          map.data.overrideStyle(feature, { icon: getIcon("blue") }); // ここはgeojsonから取得する必要あり
+          
+        // }
       });
     };
+
     // Google Maps APIがロード済みかチェック
     if ((window as any).google && (window as any).google.maps) {
       initMap();
